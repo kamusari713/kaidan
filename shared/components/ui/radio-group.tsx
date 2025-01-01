3ffbad25cc22ce5d4812e6d512f318bdf76e3f6f
kamusari1337 <ovsyannikovkostyan@gmail.com>
2025-01-01 23:06:50 +0400

    update styling

--- shared/components/ui/radio-group.tsx
+++ shared/components/ui/radio-group.tsx
@@ -13,16 +13,9 @@ RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
 
 const RadioGroupItem = React.forwardRef<React.ElementRef<typeof RadioGroupPrimitive.Item>, React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>>(({ className, ...props }, ref) => {
 	return (
-		<RadioGroupPrimitive.Item
-			ref={ref}
-			className={cn(
-				'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-				className
-			)}
-			{...props}
-		>
+		<RadioGroupPrimitive.Item ref={ref} className={cn('h-4 w-4 rounded-full border-primary border-[1px]', className)} {...props}>
 			<RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-				<Circle className="h-2.5 w-2.5 fill-current text-current" />
+				<Circle className="h-full w-full fill-primary stroke-primary" />
 			</RadioGroupPrimitive.Indicator>
 		</RadioGroupPrimitive.Item>
 	)
