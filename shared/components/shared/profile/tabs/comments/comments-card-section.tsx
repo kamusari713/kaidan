2de51481ccd70943e039bb0a63c247531f48acd4
kamusari1337 <ovsyannikovkostyan@gmail.com>
2025-01-04 13:34:41 +0400

    mokeup comments

--- shared/components/shared/profile/tabs/comments/comments-card-section.tsx
+++ shared/components/shared/profile/tabs/comments/comments-card-section.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 
 export const CommentsCardSection: React.FC = () => {
 	return (
-		<div className="flex-grow">
+		<div className="flex flex-col gap-6 flex-grow">
 			<Input placeholder="Фильтр по тексту" />
+			<div className="min-h-[140px] p-2 bg-card border shadow rounded-xl">
+				<div className="flex h-full items-center justify-center text-foreground/60">Нет комментариев</div>
+			</div>
 		</div>
 	)
 }
