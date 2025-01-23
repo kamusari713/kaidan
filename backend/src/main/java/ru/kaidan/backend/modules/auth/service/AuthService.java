3ea7871ee1baacdc3f6b1e3957e48ad990a869e8
kamusari1337 <ovsyannikovkostyan@gmail.com>
2025-01-24 00:21:29 +0400

    refactor(backend): move logic with tokens to jwtService

--- backend/src/main/java/ru/kaidan/backend/modules/auth/service/AuthService.java
+++ backend/src/main/java/ru/kaidan/backend/modules/auth/service/AuthService.java
@@ -3,7 +3,6 @@ package ru.kaidan.backend.modules.auth.service;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,14 +12,10 @@ import ru.kaidan.backend.modules.auth.DTO.CookieResponse;
 import ru.kaidan.backend.modules.auth.DTO.LoginRequest;
 import ru.kaidan.backend.modules.auth.DTO.RegisterRequest;
 import ru.kaidan.backend.modules.auth.details.CustomUserDetails;
-import ru.kaidan.backend.modules.auth.entities.TokenEntity;
 import ru.kaidan.backend.modules.auth.entities.TokenType;
-import ru.kaidan.backend.modules.auth.repositories.TokenRepository;
 import ru.kaidan.backend.modules.user.entities.RoleType;
 import ru.kaidan.backend.modules.user.entities.UserEntity;
 import ru.kaidan.backend.modules.user.repositories.UserRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -28,79 +23,10 @@ public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
-    private final TokenRepository tokenRepository;
     private final JwtService jwtService;
 
-    @Value("${jwt.accessToken.cookie-name}")
-    private String accessCookieName;
     @Value("${jwt.refreshToken.cookie-name}")
     private String refreshCookieName;
-
-    @Value("${jwt.accessToken.expiration}")
-    private long accessTokenExpiration;
-    @Value("${jwt.refreshToken.expiration}")
-    private long refreshTokenExpiration;
-
-    private void revokeAllUserTokens(UserEntity user) {
-        List<TokenEntity> validUserTokens = tokenRepository.findByUserIdAndRevokedFalse(user.getId());
-        if (validUserTokens.isEmpty()) {
-            return;
-        }
-        validUserTokens.forEach(token ->
-                token.setRevoked(true));
-        tokenRepository.saveAll(validUserTokens);
-    }
-
-    private void revokeAllUserTokens(UserEntity user, TokenType tokenType) {
-        List<TokenEntity> validUserTokens = tokenRepository.findByUserIdAndRevokedFalseAndType(user.getId(), tokenType);
-        if (validUserTokens.isEmpty()) {
-            return;
-        }
-        validUserTokens.forEach(token ->
-                token.setRevoked(true));
-        tokenRepository.saveAll(validUserTokens);
-    }
-
-    private void saveUserToken(UserEntity user, String accessToken, TokenType tokenType) {
-        TokenEntity token = TokenEntity.builder()
-                .userId(user.getId())
-                .token(accessToken)
-                .type(tokenType)
-                .revoked(false)
-                .build();
-        tokenRepository.save(token);
-    }
-
-    private CookieResponse buildCookies(String accessToken, String refreshToken) {
-        ResponseCookie accessCookie = ResponseCookie.from(accessCookieName, accessToken)
-                .httpOnly(false)
-                .secure(false)
-                .maxAge(accessTokenExpiration)
-                .path("/")
-                .sameSite("Strict")
-                .build();
-
-        ResponseCookie refreshCookie = ResponseCookie.from(refreshCookieName, refreshToken)
-                .httpOnly(false)
-                .secure(false)
-                .maxAge(refreshTokenExpiration)
-                .path("/")
-                .sameSite("Strict")
-                .build();
-
-        return new CookieResponse(accessCookie, refreshCookie);
-    }
-
-    private String getTokenFromCookies(HttpServletRequest request, String cookieName) {
-        if (request.getCookies() != null) {
-            for (var cookie : request.getCookies()) {
-                if (cookieName.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
-    }
 
     public CookieResponse registerUser(RegisterRequest registerRequest) {
         UserEntity user = new UserEntity();
@@ -113,10 +39,10 @@ public class AuthService {
         CustomUserDetails customUserDetails = new CustomUserDetails(user);
         String accessToken = jwtService.generateAccessToken(customUserDetails);
         String refreshToken = jwtService.generateRefreshToken(customUserDetails);
-        saveUserToken(user, accessToken, TokenType.ACCESS);
-        saveUserToken(user, refreshToken, TokenType.REFRESH);
+        jwtService.saveUserToken(user, accessToken, TokenType.ACCESS);
+        jwtService.saveUserToken(user, refreshToken, TokenType.REFRESH);
 
-        return buildCookies(accessToken, refreshToken);
+        return jwtService.buildCookies(accessToken, refreshToken);
     }
 
     public CookieResponse loginUser(LoginRequest authRequest) {
@@ -131,14 +57,14 @@ public class AuthService {
         CustomUserDetails customUserDetails = new CustomUserDetails(user);
         String accessToken = jwtService.generateAccessToken(customUserDetails);
         String refreshToken = jwtService.generateRefreshToken(customUserDetails);
-        revokeAllUserTokens(user);
-        saveUserToken(user, accessToken, TokenType.ACCESS);
-        saveUserToken(user, refreshToken, TokenType.REFRESH);
-        return buildCookies(accessToken, refreshToken);
+        jwtService.revokeAllUserTokens(user);
+        jwtService.saveUserToken(user, accessToken, TokenType.ACCESS);
+        jwtService.saveUserToken(user, refreshToken, TokenType.REFRESH);
+        return jwtService.buildCookies(accessToken, refreshToken);
     }
 
     public CookieResponse refreshToken(HttpServletRequest request) throws Exception {
-        String refreshToken = getTokenFromCookies(request, refreshCookieName);
+        String refreshToken = jwtService.getTokenFromCookies(request, refreshCookieName);
         if (refreshToken == null) {
             throw new Exception("Refresh token is missing");
         }
@@ -159,9 +85,9 @@ public class AuthService {
         }
 
         String newAccessToken = jwtService.generateAccessToken(customUserDetails);
-        revokeAllUserTokens(user, TokenType.ACCESS);
-        saveUserToken(user, newAccessToken, TokenType.ACCESS);
+        jwtService.revokeAllUserTokens(user, TokenType.ACCESS);
+        jwtService.saveUserToken(user, newAccessToken, TokenType.ACCESS);
 
-        return buildCookies(newAccessToken, refreshToken);
+        return jwtService.buildCookies(newAccessToken, refreshToken);
     }
 }
--- backend/src/main/java/ru/kaidan/backend/modules/auth/service/JwtService.java
+++ backend/src/main/java/ru/kaidan/backend/modules/auth/service/JwtService.java
@@ -5,23 +5,34 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import ru.kaidan.backend.modules.auth.DTO.CookieResponse;
+import ru.kaidan.backend.modules.auth.entities.TokenEntity;
+import ru.kaidan.backend.modules.auth.entities.TokenType;
+import ru.kaidan.backend.modules.auth.repositories.TokenRepository;
+import ru.kaidan.backend.modules.user.entities.UserEntity;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class JwtService {
+    private final TokenRepository tokenRepository;
     @Value("${jwt.secret}")
     private String secret;
-
+    @Value("${jwt.accessToken.cookie-name}")
+    private String accessCookieName;
     @Value("${jwt.accessToken.expiration}")
     private long accessTokenExpiration;
-
+    @Value("${jwt.refreshToken.cookie-name}")
+    private String refreshCookieName;
     @Value("${jwt.refreshToken.expiration}")
     private long refreshTokenExpiration;
 
@@ -66,5 +77,66 @@ public class JwtService {
     public boolean isTokenValid(String token, UserDetails userDetails) {
         String username = extractUsername(token);
         return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    public CookieResponse buildCookies(String accessToken, String refreshToken) {
+        ResponseCookie accessCookie = ResponseCookie.from(accessCookieName, accessToken)
+                .httpOnly(false)
+                .secure(false)
+                .maxAge(accessTokenExpiration)
+                .path("/")
+                .sameSite("Strict")
+                .build();
+
+        ResponseCookie refreshCookie = ResponseCookie.from(refreshCookieName, refreshToken)
+                .httpOnly(false)
+                .secure(false)
+                .maxAge(refreshTokenExpiration)
+                .path("/")
+                .sameSite("Strict")
+                .build();
+
+        return new CookieResponse(accessCookie, refreshCookie);
+    }
+
+    public void revokeAllUserTokens(UserEntity user) {
+        List<TokenEntity> validUserTokens = tokenRepository.findByUserIdAndRevokedFalse(user.getId());
+        if (validUserTokens.isEmpty()) {
+            return;
+        }
+        validUserTokens.forEach(token ->
+                token.setRevoked(true));
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void revokeAllUserTokens(UserEntity user, TokenType tokenType) {
+        List<TokenEntity> validUserTokens = tokenRepository.findByUserIdAndRevokedFalseAndType(user.getId(), tokenType);
+        if (validUserTokens.isEmpty()) {
+            return;
+        }
+        validUserTokens.forEach(token ->
+                token.setRevoked(true));
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void saveUserToken(UserEntity user, String accessToken, TokenType tokenType) {
+        TokenEntity token = TokenEntity.builder()
+                .userId(user.getId())
+                .token(accessToken)
+                .type(tokenType)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    public String getTokenFromCookies(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() != null) {
+            for (var cookie : request.getCookies()) {
+                if (cookieName.equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
     }
 }
